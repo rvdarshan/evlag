55d7d2a4a1194d22fea99fee4103d19b16f91e0c
rvdarshan <104909006+rvdarshan@users.noreply.github.com>
2023-02-13 10:29:09 +0530

    commit

--- evlag/src/main.js
+++ evlag/src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
-
+import router from './router';
+import axios from 'axios';
 Vue.config.productionTip = false
-
+axios.defaults.baseURL=''
+Vue.prototype.baseURL=''
 new Vue({
   vuetify,
+  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ evlag/src/router.js
@@ -0,0 +1,10 @@
+import Vue from "vue";
+import Router from "vue-router";
+Vue.use(Router);
+let router =new Router({
+    mode: 'history',
+    routers:[{
+
+    }]
+})
+export default router;
