568e82869eeb13675f9adbbabd05d38bca69cb07
shaunthomasninan <104908986+shaunthomasninan@users.noreply.github.com>
2023-02-17 11:53:56 +0530

    abc

---
+++ evlag/src/components/admin/adminPanel.vue
@@ -0,0 +1,81 @@
+<template>
+    <div>
+      <v-layout wrap style="height: 100vh; background-color: #BDBDBD;">
+        <v-flex lg2 xs2>
+          <template>
+            <v-card elevation="0" class="mx-auto" max-width="500" max-height="600">
+              <v-toolbar color="white"  >
+                
+  
+                <v-toolbar-title style="margin-left:32%">Super Admin</v-toolbar-title>
+  
+                <v-spacer></v-spacer>
+  
+                
+              </v-toolbar>
+  
+              <v-list style=" background-color: #BDBDBD;">
+                <v-list-group v-for="item in items" :key="item.title" v-model="item.active" :prepend-icon="item.action"
+                  no-action >
+                  <template v-slot:activator>
+                    <v-list-item-content>
+                      <v-list-item-title v-text="item.title" style="font-size:20px"></v-list-item-title>
+                    </v-list-item-content>
+                  </template>
+  
+                  <v-list-item v-for="child in item.items" :key="child.title">
+                    <v-list-item-content>
+                      <router-link :to="child.link" style="text-decoration:none;color:black;">
+                      <v-list-item-title v-text="child.title" style="font-size:20px"></v-list-item-title>
+                      </router-link>
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-list-group>
+              </v-list>
+              
+             
+            </v-card>
+          </template>
+        </v-flex>
+        <v-flex xs10 lg10 px-1>
+          <v-card elevation="10" style="height: 100vh;">
+  
+            <router-view></router-view>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </div>
+  </template>
+  
+  
+  
+  
+  <script>
+  export default {
+    data: () => ({
+      items: [
+        {
+          action: 'mdi-view-dashboard',
+          items: [{ title: 'Dashboard',link: 'dashBoard' },
+                  {title:'Profile',link: 'profile'}],
+          title: 'Dashboard',
+        },
+        {
+          action: 'mdi-account',
+          active: true,
+          items: [
+            { title: 'Registration',link: 'registration' },
+            { title: 'View Employee',link: 'viewEmployee' },
+            
+            
+          ],
+          title: 'Employee Management',
+        },
+        
+        
+      ],
+    }),
+      
+  }
+  </script>
+  
---
+++ evlag/src/components/admin/dashBoard.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="dashboard">
+      <h1>Dashboard</h1>
+      <div class="row">
+        <div class="col-md-6">
+          <div class="card">
+            <div class="card-header">Sales</div>
+            <div class="card-body">
+              <canvas id="sales-chart"></canvas>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="card">
+            <div class="card-header">Top Products</div>
+            <div class="card-body">
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th>Product Name</th>
+                    <th>Units Sold</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Product A</td>
+                    <td>1000</td>
+                  </tr>
+                  <tr>
+                    <td>Product B</td>
+                    <td>800</td>
+                  </tr>
+                  <tr>
+                    <td>Product C</td>
+                    <td>600</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import Chart from 'chart.js';
+  
+  export default {
+    mounted() {
+      // Sales Chart
+      const salesData = [1000, 2000, 1500, 3000, 2500, 2000, 1500];
+      const salesLabels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+      const salesChart = new Chart(document.getElementById('sales-chart'), {
+        type: 'line',
+        data: {
+          labels: salesLabels,
+          datasets: [
+            {
+              label: 'Sales',
+              data: salesData,
+              borderColor: 'blue',
+              borderWidth: 2,
+              fill: false,
+            },
+          ],
+        },
+        options: {
+          responsive: true,
+          maintainAspectRatio: false,
+          legend: {
+            display: false,
+          },
+        },
+      });
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .dashboard {
+    margin: 20px;
+  }
+  .card {
+    margin-bottom: 20px;
+  }
+  </style>
+  
