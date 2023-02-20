6261e9b006b60fca4f6ebb934f025fa9729c5d43
rvdarshan <104909006+rvdarshan@users.noreply.github.com>
2023-02-20 11:45:58 +0530

    commit

--- evlag/src/components/superadmin/dashBoard.vue
+++ evlag/src/components/superadmin/dashBoard.vue
@@ -2,7 +2,14 @@
     <div>
         <v-layout wrap justify-center>
             <v-flex lg12>
-                <v-img src="../../assets/images/logo.png" width="15%" style="margin-left:40%; margin-top:5%"></v-img>
+                <v-img src="../../assets/images/3873336.jpg" height="45vh">
+                    <v-layout wrap justify-center>
+                        <v-flex lg12>
+                            <v-img src="../../assets/images/logo-no-background.png" width="15%" style="margin-left:42.8%; margin-top:5%"></v-img>
+                        </v-flex>
+                    </v-layout>
+                </v-img>
+                
             </v-flex>
         </v-layout>
     </div>
---
+++ evlag/src/components/superadmin/profileSuper.vue
@@ -0,0 +1,73 @@
+<template>
+    <div>
+        <v-layout wrap justify-center>
+            <v-flex lg12 scroll>
+                <v-img src="" height="100vh">
+                    <v-layout wrap justify-center>
+                        <v-flex lg12>
+                            <h2 style="font-family:typ;font-size:40px; text-align: center;margin-top:2%"><u>PROFILE</u></h2>
+                        </v-flex>
+                    </v-layout>
+                    <v-layout wrap justify-center>
+                        <v-flex lg6>
+                            <v-layout wrap justify-center>
+                                <v-flex lg12>
+                                    <p class="nju" style="margin-left:41.5%">NAME</p>
+                                </v-flex>
+                            </v-layout>
+                            <v-layout wrap justify-center>
+                                <v-flex lg12>
+                                    <p class="head">ACHAL DOMINIC</p>
+                                </v-flex>
+                            </v-layout>
+                        </v-flex>
+                        <v-flex lg6>
+                            <v-layout wrap justify-center>
+                                <v-flex lg12>
+                                    <p class="nju" style="margin-left:45%">PHONE</p>
+                                </v-flex>
+                            </v-layout>
+                            <v-layout wrap justify-center>
+                                <v-flex lg12>
+                                    <p class="head" style="margin-left: 38%">7896254736</p>
+                                </v-flex>
+                            </v-layout>
+                        </v-flex>
+                    </v-layout>
+                    <v-layout wrap justify-center>
+                        <v-flex lg12 >
+                            <v-layout wrap justify-center>
+                                <v-flex lg12>
+                                    <p class="njh" style="margin-left:47%">EMAIL</p>
+                                </v-flex>
+                            </v-layout>
+                            <v-layout wrap justify-center>
+                                <v-flex lg12>
+                                    <p class="head" style="margin-left:35%">achaldomman999@gmail.com</p>
+                                </v-flex>
+                            </v-layout>
+                        </v-flex>
+                    </v-layout>
+                    <v-layout wrap justify-center>
+                        <v-flex lg12 style="margin-top:8%">
+                              <v-row
+                                align="center"
+                                justify="space-around"
+                            >
+                                <v-btn
+                                tile
+                                color="success"
+                                >
+                                <v-icon left>
+                                    mdi-pencil
+                                </v-icon>
+                                UPDATE PASSWORD
+                                </v-btn>
+                            </v-row>
+                        </v-flex>
+                    </v-layout>
+                </v-img>
+            </v-flex>
+        </v-layout>
+    </div>
+</template>
