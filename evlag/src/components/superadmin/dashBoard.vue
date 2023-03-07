b2943eb16070d5926429dd0bad4a68a36a883113
rvdarshan <104909006+rvdarshan@users.noreply.github.com>
2023-03-07 14:46:46 +0530

    commit

--- evlag/src/components/superadmin/dashBoard.vue
+++ evlag/src/components/superadmin/dashBoard.vue
@@ -8,32 +8,11 @@
                             <v-img class="my-element" src="../../assets/images/logo-no-background.png" width="15%" style="margin-left:42.8%; margin-top:15%"></v-img>
                         </v-flex>
                     </v-layout>
-                    <v-layout wrap justify-center>
-                        <v-flex lg12>
-                            <h1 class="h1-element" align="center">WHENEVER YOU VISIT, LET THE PLACE YOU VISIT REMEMBERS YOU WELL!</h1>
-                        </v-flex>
-                    </v-layout>
-                    <v-layout wrap justify-center>
-                        <v-flex lg12 style="margin-top:10%">
-                            <v-layout wrap justify-center>
-                                <v-flex lg3>
-                                    <v-card elevation="10" width="250px" height="150px" color="grey" >
-                                        
-                                    </v-card>
-                                </v-flex>
-                            </v-layout>
-                        </v-flex>
-                    </v-layout>
+                    
                 </v-img>
                 
             </v-flex>
         </v-layout>
     </div>
 </template>
-<script>
-import 'animate.css';
-export default {
-    
-}
-</script>
-YOUR NETWORK IS YOUR NET WORTH
+
